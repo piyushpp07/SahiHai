ef95d901d3271c8be9981e0792bcc3e274ae2bdd
piyushpp07 <piyushpar7@gmail.com>
2026-01-04 15:20:13 +0530

    simplify: remove api folder and use direct Node.js deployment
    - Revert vercel.json to minimal configuration
    - Use dist/index.js as main entry point
    - Vercel will automatically detect Node.js app and handle routing
    - Remove unnecessary api/index.js wrapper

--- server/api/index.js
+++
@@ -1,5 +0,0 @@
-// Vercel serverless function handler
-// This simply exports the Express app from the compiled dist folder
-const app = require("../dist/index.js").default || require("../dist/index.js");
-
-module.exports = app;
